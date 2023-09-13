f612f2805adaed1bd7f195342087abeaa82a880b
CCat <103255096+autobotoptimusprime@users.noreply.github.com>
2023-09-13 22:07:45 +0800

    Update base64.hpp

--- base64.hpp
+++ base64.hpp
@@ -7,7 +7,6 @@
 #include <ranges>
 #endif
 
-namespace ccat {
 namespace base64 {
 
 using byte = unsigned char;
@@ -16,8 +15,30 @@ namespace detail {
     
 template<typename...>
 using void_t = void;
+
 template<bool p, typename T = void>
 using enable_if_t = typename std::enable_if<p, T>::type;
+
+template<bool cond, typename L, typename R>
+using conditional_t = typename std::conditional<cond, L, R>::type;
+
+template<typename T, typename = void>
+struct is_forward_iter : std::false_type {};
+
+template<typename T>
+struct is_forward_iter<T, enable_if_t<std::is_pointer<T>::value>> : std::true_type {};
+
+template<typename T>
+struct is_forward_iter<T, enable_if_t<std::is_base_of<std::forward_iterator_tag, typename T::iterator_category>::value>> : std::true_type {};
+
+template<typename T, typename = void>
+struct is_output_iter : std::false_type {};
+
+template<typename T>
+struct is_output_iter<T, enable_if_t<std::is_pointer<T>::value>> : std::true_type {};
+
+template<typename T>
+struct is_output_iter<T, enable_if_t<std::is_base_of<std::output_iterator_tag, typename T::iterator_category>::value>> : std::true_type {};
 
 template<typename T, typename = void>
 struct is_byte_type : std::false_type {};
@@ -32,19 +53,36 @@ struct is_byte_type<T, enable_if_t<
 #endif
     >> : std::true_type {};
 
+template<typename T, typename = void>
+struct is_byte_type_iter : std::false_type {};
+
+template<typename T>
+struct is_byte_type_iter<T, enable_if_t<std::is_pointer<T>::value && is_byte_type<typename std::remove_pointer<T>::type>::value>> : std::true_type {};
+
+template<typename T>
+struct is_byte_type_iter<T, enable_if_t<is_byte_type<typename T::value_type>::value>> : std::true_type {};
+
+template<typename T, typename = void>
+struct is_rdonly_byte_type_iter : std::false_type {};
+
+template<typename T>
+struct is_rdonly_byte_type_iter<T, enable_if_t<std::is_pointer<T>::value && is_byte_type<typename std::remove_cv<typename std::remove_pointer<T>::type>::type>::value>> : std::true_type {};
+
+template<typename T>
+struct is_rdonly_byte_type_iter<T, enable_if_t<is_byte_type<typename std::remove_cv<typename T::value_type>::type>::value>> : std::true_type {};
+
+
 #if defined(__cplusplus) && __cplusplus >= 202002L || defined(_MSVC_LANG) && _MSVC_LANG >= 202002L
 
 template<typename T>
-struct is_range {
-    static constexpr bool value = std::ranges::range<T>;
-};
+struct is_range : std::bool_constant<std::ranges::range<T>> {};
 
 #else
 
 template<typename T, typename = void>
 struct is_range : std::false_type {};
 template<typename T>
-struct is_range<T, void_t<decltype(std::begin(std::declval<T>()), std::end(std::declval<T>()), 0)>> : std::true_type {};
+struct is_range<T, void_t<decltype(std::begin(std::declval<T&>()), std::end(std::declval<T&>()), 0)>> : std::true_type {};
 
 #endif
 
@@ -52,12 +90,13 @@ struct is_range<T, void_t<decltype(std::begin(std::declval<T>()), std::end(std::
 
 template<typename ForwardIt, typename OutIt>
 auto encode_to(ForwardIt first, ForwardIt second, OutIt target) -> void {
-    static_assert(std::is_base_of<std::forward_iterator_tag, typename ForwardIt::iterator_category>::value, "type `ForwardIt` must be a forward-iterator type");
+    static_assert(detail::is_forward_iter<ForwardIt>::value, "type `ForwardIt` must be a forward-iterator type");
     static_assert(
-            std::is_base_of<std::forward_iterator_tag, typename OutIt::iterator_category>::value ||
-                    std::is_base_of<std::output_iterator_tag, typename OutIt::iterator_category>::value,
-            "type `OutIt` must be a forward-iterator or output-iterator type");
-    static_assert(detail::is_byte_type<typename ForwardIt::value_type>::value, "the type of value in the range [first, second) must be `char`, `unsigned char`, `signed char`, `byte` or `std::byte`");
+        detail::is_forward_iter<OutIt>::value ||
+        detail::is_output_iter<OutIt>::value,
+        "type `OutIt` must be a forward-iterator or output-iterator type"
+    );
+    static_assert(detail::is_rdonly_byte_type_iter<ForwardIt>::value, "the type of value in the range [first, second) must be `char`, `unsigned char`, `signed char`, `byte` or `std::byte`");
     static const std::array<char, 65> table_{"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/"};
     size_t flag_{};
     byte prev{};
@@ -117,12 +156,13 @@ auto encode(const Range& rng_) ->std::string {
 
 template<typename ForwardIt, typename OutIt>
 auto decode_to(ForwardIt first, ForwardIt second, OutIt target) -> void {
-    static_assert(std::is_base_of<std::forward_iterator_tag, typename ForwardIt::iterator_category>::value, "type `ForwardIt` must be a forward-iterator type");
+    static_assert(detail::is_forward_iter<ForwardIt>::value, "type `ForwardIt` must be a forward-iterator type");
     static_assert(
-            std::is_base_of<std::forward_iterator_tag, typename OutIt::iterator_category>::value ||
-                    std::is_base_of<std::output_iterator_tag, typename OutIt::iterator_category>::value,
-            "type `OutIt` must be a forward-iterator or output-iterator type");
-    static_assert(detail::is_byte_type<typename ForwardIt::value_type>::value, "the type of value in the range [first, second) must be `char`, `unsigned char`, `signed char`, `byte` or `std::byte`");
+        detail::is_forward_iter<OutIt>::value ||
+        detail::is_output_iter<OutIt>::value,
+        "type `OutIt` must be a forward-iterator or output-iterator type"
+    );
+    static_assert(detail::is_byte_type_iter<ForwardIt>::value, "the type of value in the range [first, second) must be `char`, `unsigned char`, `signed char`, `byte` or `std::byte`");
     size_t flag_{};
     std::array<byte, 3> buff_{};
     for (auto it = first; it != second && *it != '='; ++it) {
@@ -170,5 +210,4 @@ auto decode_to(const Range& rng_, OutIt target) ->void {
     decode_to(std::begin(rng_), std::end(rng_), target);
 }
 
-}
 }
