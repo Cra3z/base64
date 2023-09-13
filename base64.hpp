9c2232a65d7b35d14d7d3bc328670d1d39d209c5
ccat <harlanzou2021@outlook.com>
2023-09-13 20:36:46 +0800

    upload file

---
+++ base64.hpp
@@ -0,0 +1,174 @@
+#pragma once
+#include <type_traits>
+#include <string>
+#include <array>
+
+#if defined(__cplusplus) && __cplusplus >= 202002L || defined(_MSVC_LANG) && _MSVC_LANG >= 202002L
+#include <ranges>
+#endif
+
+namespace ccat {
+namespace base64 {
+
+using byte = unsigned char;
+
+namespace detail {
+    
+template<typename...>
+using void_t = void;
+template<bool p, typename T = void>
+using enable_if_t = typename std::enable_if<p, T>::type;
+
+template<typename T, typename = void>
+struct is_byte_type : std::false_type {};
+template<typename T>
+struct is_byte_type<T, enable_if_t<
+    std::is_same<char, T>::value ||
+    std::is_same<signed char, T>::value ||
+    std::is_same<unsigned char, T>::value ||
+    std::is_same<byte, T>::value
+#if defined(__cplusplus) && __cplusplus >= 201703L || defined(_MSVC_LANG) && _MSVC_LANG >= 201703L
+    || std::is_same<std::byte, T>::value
+#endif
+    >> : std::true_type {};
+
+#if defined(__cplusplus) && __cplusplus >= 202002L || defined(_MSVC_LANG) && _MSVC_LANG >= 202002L
+
+template<typename T>
+struct is_range {
+    static constexpr bool value = std::ranges::range<T>;
+};
+
+#else
+
+template<typename T, typename = void>
+struct is_range : std::false_type {};
+template<typename T>
+struct is_range<T, void_t<decltype(std::begin(std::declval<T>()), std::end(std::declval<T>()), 0)>> : std::true_type {};
+
+#endif
+
+}
+
+template<typename ForwardIt, typename OutIt>
+auto encode_to(ForwardIt first, ForwardIt second, OutIt target) -> void {
+    static_assert(std::is_base_of<std::forward_iterator_tag, typename ForwardIt::iterator_category>::value, "type `ForwardIt` must be a forward-iterator type");
+    static_assert(
+            std::is_base_of<std::forward_iterator_tag, typename OutIt::iterator_category>::value ||
+                    std::is_base_of<std::output_iterator_tag, typename OutIt::iterator_category>::value,
+            "type `OutIt` must be a forward-iterator or output-iterator type");
+    static_assert(detail::is_byte_type<typename ForwardIt::value_type>::value, "the type of value in the range [first, second) must be `char`, `unsigned char`, `signed char`, `byte` or `std::byte`");
+    static const std::array<char, 65> table_{"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/"};
+    size_t flag_{};
+    byte prev{};
+    for (auto it = first; it != second; ++it) {
+        auto value = static_cast<byte>(*it);
+        switch (flag_) {
+        case 0:
+            *target = table_[value >> 2];
+            prev = byte(value << 6) >> 2;
+            break;
+        case 1:
+            *target = table_[prev | (value >> 4)];
+            prev = byte(value << 4) >> 2;
+            break;
+        case 2:
+            *target = table_[prev | (value >> 6)];
+            ++target;
+            *target = table_[byte(value << 2) >> 2];
+            prev = static_cast<byte>(0);
+            break;
+        }
+        ++target;
+        ++flag_;
+        flag_ %= 3;
+    }
+    if (flag_) {
+        *target = table_[prev];
+        ++target;
+    }
+    for (auto i = flag_; i > 0 && i < 3; ++i) {
+        *target = '=';
+        ++target;
+    }
+}
+
+template<typename Range, typename OutIt>
+auto encode_to(const Range& rng_, OutIt target) -> void {
+    static_assert(detail::is_range<Range>::value, "type `Range` must be a range type: [begin, end)");
+    encode_to(std::begin(rng_), std::end(rng_), target);
+}
+
+template<typename ForwardIt>
+auto encode(ForwardIt first, ForwardIt second) ->std::string {
+    std::string res{};
+    encode_to(first, second, std::back_inserter(res));
+    return res;
+}
+
+template<typename Range>
+auto encode(const Range& rng_) ->std::string {
+    static_assert(detail::is_range<Range>::value, "type `Range` must be a range type: [begin, end)");
+    std::string res{};
+    encode_to(std::begin(rng_), std::end(rng_), std::back_inserter(res));
+    return res;
+}
+
+
+template<typename ForwardIt, typename OutIt>
+auto decode_to(ForwardIt first, ForwardIt second, OutIt target) -> void {
+    static_assert(std::is_base_of<std::forward_iterator_tag, typename ForwardIt::iterator_category>::value, "type `ForwardIt` must be a forward-iterator type");
+    static_assert(
+            std::is_base_of<std::forward_iterator_tag, typename OutIt::iterator_category>::value ||
+                    std::is_base_of<std::output_iterator_tag, typename OutIt::iterator_category>::value,
+            "type `OutIt` must be a forward-iterator or output-iterator type");
+    static_assert(detail::is_byte_type<typename ForwardIt::value_type>::value, "the type of value in the range [first, second) must be `char`, `unsigned char`, `signed char`, `byte` or `std::byte`");
+    size_t flag_{};
+    std::array<byte, 3> buff_{};
+    for (auto it = first; it != second && *it != '='; ++it) {
+        auto value = static_cast<byte>(*it);
+        if (value <= '/') value = value == '+' ? 62 : 63;
+        else if (value <= '9') value = value - '0' + 52;
+        else if (value <= 'Z') value = value - 'A';
+        else if (value <= 'z') value = value - 'a' + 26;
+        switch (flag_)
+        {
+        case 0:
+            buff_[0] = byte(value << 2);
+            break;
+        case 1:
+            buff_[0] += value >> 4;
+            buff_[1] = value << 4;
+            break;
+        case 2:
+            buff_[1] += value >> 2;
+            buff_[2] = value << 6;
+            break;
+        case 3:
+            buff_[2] += value;
+            for (auto c : buff_) {
+                *target = c;
+                ++target;
+            }
+            buff_.fill(0);
+            break;
+        }
+        ++flag_;
+        flag_ %= 4;
+    }
+    if (flag_) {
+        for (auto c : buff_) {
+            *target = c;
+            ++target;
+        }
+    }
+}
+
+template<typename Range, typename OutIt>
+auto decode_to(const Range& rng_, OutIt target) ->void {
+    static_assert(detail::is_range<Range>::value, "type `Range` must be a range type: [begin, end)");
+    decode_to(std::begin(rng_), std::end(rng_), target);
+}
+
+}
+}
